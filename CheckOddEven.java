dbe47429871a584580ffbb787703b94f18681abc
sakshi thakur <122427915+sakshiThakur-7@users.noreply.github.com>
2023-10-20 01:39:46 +0530

    CheckOddEven.java

---
+++ CheckOddEven.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Evenodd{
+    public static void main(String[] args) {
+        Scanner Even=new Scanner(System.in);
+        System.out.println(x:"enter any number");
+        int a =Even.nextInt();
+        System.out.println((a%2==0)?"even":"odd");
+        
+        Scanner.close();
+        }
+    }
