494430d0fa7c93678e4ba634f6dff61764249dbf
sakshi thakur <122427915+sakshiThakur-7@users.noreply.github.com>
2023-10-20 01:55:19 +0530

    CircleArea.java

---
+++ CircleArea.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Rectangle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter any two different number");
+        double a = sc.nextDouble();
+        double b = 3.14;
+        double c = b*(a*a);
+
+        System.out.println("area of circle = "+c);
+
+    }
+}
