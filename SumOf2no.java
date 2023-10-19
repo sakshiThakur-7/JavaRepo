e47cdde1a343bd74a7a6b39073586d58460472d5
sakshi thakur <122427915+sakshiThakur-7@users.noreply.github.com>
2023-10-20 00:26:58 +0530

    SumOf2no.java

---
+++ SumOf2no.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Sum{
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter two number");
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int c=a+b;
+        System.out.println("sum = "+c);
+        
+    }
+}
