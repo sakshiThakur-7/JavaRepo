85811bfed7a4f121141861c33cf3bcc0f457b6e2
sakshi thakur <122427915+sakshiThakur-7@users.noreply.github.com>
2023-10-20 00:56:57 +0530

    Swap.java

--- Swap.java
+++ Swap.java
@@ -1,2 +1,17 @@
+import java.util.Scanner;
+
 public class Swap {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter any two different number");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println("before: a = " + a + " and before b = " + b);
+        int f = a * b;
+        int g = f / a;
+        int h = f / b;
+        System.out.println("after: a = " + g + " and after b = " + h);
+
+    }
 }
