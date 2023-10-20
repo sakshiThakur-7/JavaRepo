c5e685d185d09de0001e2c4b57be39fe1ad80ead
sakshi thakur <122427915+sakshiThakur-7@users.noreply.github.com>
2023-10-20 21:08:25 +0530

    Add files via upload

---
+++ ConvertUpperToLowerCase.java
@@ -0,0 +1,17 @@
+public class ConvertUpperToLowerCase {
+    public static void main(String[] args) {
+        String s = "Geeks for Geeks";
+ 
+        // Converting string s to lowercase 
+
+        String s2 = s.toLowerCase();
+ 
+    //    Converting string s to uppercase
+
+        String s3 = s.toUpperCase();
+    
+        System.out.println(s2);
+
+        System.out.println(s3);
+    }
+}
