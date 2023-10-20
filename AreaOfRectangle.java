ba0ca029646a3954e824b14c74a4d4e4c12cbed1
sakshi thakur <122427915+sakshiThakur-7@users.noreply.github.com>
2023-10-20 21:07:34 +0530

    Add files via upload

---
+++ AreaOfRectangle.java
@@ -0,0 +1,11 @@
+public class AreaOfRectangle {
+    public static void main(String[] args) {
+        double h =5;
+        double b=10;
+
+        double result = (h*b)/2;
+
+        System.out.println("Area of tringle is : " +result);
+
+    }
+}
