b6060602494d8c256d09acaec7e407e7d7b893f9
sakshi thakur <122427915+sakshiThakur-7@users.noreply.github.com>
2023-10-20 21:09:01 +0530

    Add files via upload

---
+++ PerimeterOfRectangle.java
@@ -0,0 +1,11 @@
+public class PerimeterOfRectangle {
+    public static void main(String[] args) {
+        float a =7;
+        float b = 5;
+        float c = 9;
+
+        float result = a+ b + c;
+
+        System.out.println("Perimeter of rectangle is :" + result);
+    }
+}
